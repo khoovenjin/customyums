f7c99caccffdfea34d335efd74241eee43f83607
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-07-24 15:17:56 +0800

    Changed Color Pallet to Beige Theme

--- frontend/app/config/colors.js
+++ frontend/app/config/colors.js
@@ -1,12 +1,15 @@
 export default {
-  primary: "#E1C5AC",
-  secondary: "#CFA052",
+  primary: "#D6B9A7",
+  secondary: "#B0907B",
   black: "#000",
   white: "#FFF",
-  brown: "#523629",
-  cream: "#DBD5C9",
+  primary_brown: "#967B68",
+  secondary_brown: "#78655E",
+  primary_grey: "#56514E",
   green: "#4FD966",
-  pink: "#FEA8FF",
+  pink: "#FF82AC",
+  primary_red: "#F42C38",
+  secondary_red: "#EB0301",
   transparent: "rgba(0, 0, 0, 0)",
   translucent: "rgba(0, 0, 0, 0.5)"
 }

ae807d0e407dbd641005649e6bc23b0c53decc34
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-07-24 15:19:20 +0800

    Created styles to export config

--- frontend/app/config/styles.js
+++ frontend/app/config/styles.js
@@ -1,9 +1,11 @@
 import { Dimensions, Platform } from "react-native";
 
 import colors from "./colors";
+import type_colors from "./type_colors";
 
 export default {
   colors,
+  type_colors,
   text: {
     color: colors.black,
     fontSize: 18,

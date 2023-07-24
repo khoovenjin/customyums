505524a65b2641c2df96ea8bac46a9adecedcc28
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-07-24 15:18:25 +0800

    Created Sample Data for dev-purpose

---
+++ frontend/app/config/data.js
@@ -0,0 +1,84 @@
+export const sampleTags = [
+  {
+    title: "Asian"
+  },
+  {
+    title: "Indian"
+  },
+  {
+    title: "Spicy"
+  },
+  {
+    title: "Curry"
+  },
+  {
+    title: "Hot"
+  },
+  {
+    title: "Lactose"
+  }
+];
+
+export const sampleDate = [
+  {
+    day: "Mon",
+    date: "2/7"
+  },
+  {
+    day: "Tue",
+    date: "3/7"
+  },
+  {
+    day: "Wed",
+    date: "4/7"
+  },
+  {
+    day: "Thu",
+    date: "5/7"
+  },
+  {
+    day: "Fri",
+    date: "6/7"
+  },
+  {
+    day: "Sat",
+    date: "7/7"
+  },
+  {
+    day: "Sun",
+    date: "8/7"
+  },
+  {
+    day: "Mon",
+    date: "9/7"
+  },
+  {
+    day: "Tue",
+    date: "10/7"
+  },
+  {
+    day: "Wed",
+    date: "11/7"
+  },
+  {
+    day: "Thu",
+    date: "12/7"
+  },
+  {
+    day: "Fri",
+    date: "13/7"
+  },
+  {
+    day: "Sat",
+    date: "14/7"
+  },
+  {
+    day: "Sun",
+    date: "15/7"
+  }
+]
+
+export const sampleSelect = [
+  {label: 'This Week', value: 'current'},
+  {label: 'Next Week', value: 'next'}
+]

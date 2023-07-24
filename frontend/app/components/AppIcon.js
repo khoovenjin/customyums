a1d80c108f7a568423e6565f7ac11a39d8949533
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-07-24 15:32:11 +0800

    Added onPress effect
    Added TouchableWithoutFeedback

--- frontend/app/components/AppIcon.js
+++ frontend/app/components/AppIcon.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { TouchableWithoutFeedback, View } from 'react-native';
 
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
@@ -8,22 +8,29 @@ import defaultStyles from '../config/styles';
 function AppIcon({
     backgroundColor = defaultStyles.colors.white,
     color = defaultStyles.colors.black,
+    containerStyle,
     name,
+    onPress,
     size = 40
 }) {
     const halfSize = size/2;
 
     return (
-        <View style={{
-            alignItems: "center",
-            backgroundColor,
-            borderRadius: halfSize,
-            height: size,
-            justifyContent: "center",
-            width: size
-        }}>
-            <MaterialCommunityIcons name={ name } color={ color } size={ halfSize }/>
-        </View>
+        <TouchableWithoutFeedback onPress={ onPress }>
+            <View style={[
+                {
+                    alignItems: "center",
+                    backgroundColor,
+                    borderRadius: halfSize,
+                    height: size,
+                    justifyContent: "center",
+                    width: size
+                },
+                containerStyle
+            ]}>
+                <MaterialCommunityIcons name={ name } color={ color } size={ halfSize }/>
+            </View>
+        </TouchableWithoutFeedback>
     );
 }
 

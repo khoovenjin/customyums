24ad611f83cf6c8c5f6eb63419d6fd45c7862fe6
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-07-25 09:04:05 +0800

    Sync color to borderColor and fix alignments

--- frontend/app/components/AppTag.js
+++ frontend/app/components/AppTag.js
@@ -1,28 +1,37 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
+import AppText from './AppText';
 import defaultStyles from '../config/styles';
 
 function AppTag({
   color = defaultStyles.colors.white,
+  textColor = defaultStyles.colors.black,
   title
 }) {
   return (
-    <View style={{ ...styles.container, backgroundColor: color }}>
-      <AppText style={ styles.text }>{ title }</AppText>
+    <View style={[ styles.container, { backgroundColor: color, borderColor: color }]}>
+      <AppText style={[ defaultStyles.text, styles.text, { color: textColor } ]}>
+        { title }
+      </AppText>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginRight: 20
+    alignSelf: 'flex-start',
+    borderRadius: 20,
+    borderWidth: 2,
+    marginRight: 5,
+    marginVertical: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 5
   },
   text: {
-    fontSize: 12
+    alignSelf: 'flex-start',
+    fontSize: 12,
+    fontWeight: 'bold'
   }
 })
 

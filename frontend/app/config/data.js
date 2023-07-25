295df631eb28e3859400ba5871e8abe041ad7203
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-07-25 09:05:54 +0800

    Re-format tags, and added pantryButtons, upcomingMeals, and ingredients

--- frontend/app/config/data.js
+++ frontend/app/config/data.js
@@ -1,22 +1,10 @@
 export const sampleTags = [
-  {
-    title: "Asian"
-  },
-  {
-    title: "Indian"
-  },
-  {
-    title: "Spicy"
-  },
-  {
-    title: "Curry"
-  },
-  {
-    title: "Hot"
-  },
-  {
-    title: "Lactose"
-  }
+  "Asian",
+  "Indian",
+  "Spicy",
+  "Curry",
+  "Hot",
+  "Lactose"
 ];
 
 export const sampleDate = [
@@ -79,6 +67,151 @@ export const sampleDate = [
 ]
 
 export const sampleSelect = [
-  {label: 'This Week', value: 'current'},
-  {label: 'Next Week', value: 'next'}
+  {
+    label: 'This Week',
+    value: 'current'
+  },
+  {
+    label: 'Next Week',
+    value: 'next'
+  }
+]
+
+export const sampleUpcomingMeals = [
+  {
+    day: "Mon",
+    date: "2/7",
+    meals: [
+      {
+        type: "Breakfast",
+        recipe: [
+          {
+            image: require('../assets/tikka-masala.jpg'),
+            title: "Kale panzanella",
+            description: "This salad is part marinated kale and part and panzanella.",
+            tags: ["Dessert", "Breakfast", "Main-Dish", "Vegan", "Glutten Free"]
+          }
+        ]
+      },
+      {
+        type: "Lunch",
+        recipe: [
+          {
+            image: require('../assets/tikka-masala.jpg'),
+            title: "Chicken Chop with Black Pepper Sauce",
+            description: "Delicious grilled or pan-fried marinated chicken covered in a rich, bold black pepper sauce.",
+            tags: ["Main-Dish", "Western"]
+          },
+          {
+            image: require('../assets/tikka-masala.jpg'),
+            title: "Chocolate-Hazelnut Icebox Cake",
+            description: "Icebox cakes are the ultimate make-ahead dessert: They taste better after the cookies have had a full day to soften.",
+            tags: ["Dessert", "Nuts", "Chocolate"]
+          }
+        ]
+      },
+      {
+        type: "Tea Time",
+        recipe: [
+          {
+            image: require('../assets/tikka-masala.jpg'),
+            title: "Glaze Doughnuts",
+            description: "A small cake of sweetened or, sometimes, unsweetened dough fried in deep fat, typically shaped like a ring or, when prepared with a filling, a ball.",
+            tags: ["Dessert", "Light", "Sweet"]
+          }
+        ]
+      },
+      {
+        type: "Dinner",
+        recipe: [
+          {
+            image: require('../assets/tikka-masala.jpg'),
+            title: "Pasta Carbonara",
+            description: "A pasta dish made with eggs, hard cheese, cured pork, and black pepper.",
+            tags: ["Main-Dish", "Creamy", "Italian"]
+          },
+          {
+            image: require('../assets/tikka-masala.jpg'),
+            title: "Vanilla Crème Brûlée",
+            description: "A decadent French dessert consisting of a custard base and a caramelized sugar crust.",
+            tags: ["Dessert", "Sweet", "French"]
+          }
+        ]
+      },
+      {
+        type: "Supper",
+        recipe: []
+      }
+    ]
+  }
+]
+
+export const pantryButton = [
+  {
+    icon: "plus",
+    label: "Create"
+  },
+  {
+    icon: "trash-can",
+    label: "Delete"
+  }
+]
+
+export const sampleIngredients = [
+  {
+    name: "Garlic",
+    amount: "78"
+  },
+  {
+    name: "Pepper",
+    amount: "20"
+  },
+  {
+    name: "Salt",
+    amount: "30"
+  },
+  {
+    name: "Rice",
+    amount: "1000"
+  },
+  {
+    name: "Milk",
+    amount: "700"
+  },
+  {
+    name: "Corn Flour",
+    amount: "480"
+  },
+  {
+    name: "Heavy Cream",
+    amount: "370"
+  },
+  {
+    name: "Beef Stock",
+    amount: "790"
+  },
+  {
+    name: "Chilli",
+    amount: "40"
+  },
+  {
+    name: "Garam Masala",
+    amount: "20"
+  },
+  {
+    name: "Sugar",
+    amount: "400"
+  },
+  {
+    name: "Soy Sauce",
+    amount: "385"
+  },
+  {
+    name: "Dark Soy Sauce",
+    amount: "210"
+  },
+  {
+    name: "Oyster Sauce",
+    amount: "175"
+  }
 ]

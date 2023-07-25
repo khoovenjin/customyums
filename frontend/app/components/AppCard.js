81f0e9fe6c64aa98cd6deaccf950012c3a72196a
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-07-25 23:35:22 +0800

    Added customStyle params, swipeable-right gesture-handler, refactor design to include image, title, description, and tags

--- frontend/app/components/AppCard.js
+++ frontend/app/components/AppCard.js
@@ -1,33 +1,99 @@
 import React from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import { FlatList, TouchableWithoutFeedback, View, StyleSheet } from 'react-native';
+
+import { GestureHandlerRootView, Swipeable } from "react-native-gesture-handler";
 
 import AppImage from './AppImage';
+import AppTag from './AppTag';
 import AppText from './AppText';
+import defaultStyles from '../config/styles';
 
 function AppCard({
+  containerStyle,
   image,
+  imageHeight = 150,
+  imageWidth = '100%',
+  onPress,
   title,
+  titleStyle,
   description,
-  tags
+  descriptionStyle,
+  tags,
+  renderRightActions
 }) {
   return (
-    <View style={ styles.container }>
-      <AppImage image={ image } />
-      <View style={ styles.detailsContainer }>
-        <AppText>{ title }</AppText>
-        <AppText>{ description }</AppText>
-        
-      </View>
-    </View>
+    <GestureHandlerRootView>
+      <Swipeable
+        containerStyle={ styles.swipeable }
+        renderRightActions={ renderRightActions }
+        overshootRight={ false }
+      >
+        <TouchableWithoutFeedback onPress={ onPress }>
+          <View style={[ styles.container, containerStyle ]}>
+            <AppImage image={ image } height={ imageHeight } width={ imageWidth } />
+            <View style={ styles.detailsContainer }>
+              <AppText style={[
+                defaultStyles.text,
+                styles.text,
+                styles.title,
+                titleStyle
+              ]}>
+                { title }
+              </AppText>
+              <AppText
+                numberOfLines={ 2 }
+                style={[
+                  defaultStyles.text,
+                  styles.text,
+                  styles.description,
+                  descriptionStyle
+                ]}>
+                { description }
+              </AppText>
+              { tags &&
+                <FlatList
+                  numColumns={ tags.length }
+                  data={ tags }
+                  keyExtractor={( item, index ) => index.toString()}
+                  renderItem={({ item }) =>
+                    <AppTag
+                      title={ item }
+                      color={ defaultStyles.colors.secondary }
+                      textColor={ defaultStyles.colors.white }
+                    />
+                  }
+                />
+              }
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Swipeable>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-
+    backgroundColor: defaultStyles.colors.white,
+    overflow: 'hidden'
   },
   detailsContainer: {
-
+    padding: 10
+  },
+  swipeable: {
+    backgroundColor: defaultStyles.colors.primary_red,
+    borderColor: defaultStyles.colors.primary_brown,
+    borderRadius: 15,
+    borderWidth: 2
+  },
+  text: {
+    color: defaultStyles.colors.primary_brown
+  },
+  title: {
+    fontWeight: 'bold'
+  },
+  description: {
+    fontSize: 13
   }
 })
 

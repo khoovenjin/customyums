ad962549337b2e2996069f94a7cc27c95d8b6fb9
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-07-29 22:31:34 +0800

    Defined dietary model

--- backend/models/dietaryModel.js
+++ backend/models/dietaryModel.js
@@ -3,11 +3,15 @@ import mongoose from "mongoose";
 import { dbTables } from "../config/config.js";
 
 const DietarySchema = new mongoose.Schema({
-  
+  dietary_id: mongoose.Schema.Types.ObjectId,
+  user_id: mongoose.Schema.Types.ObjectId,
+  meal: String,
+  date: Date,
+  recipes: [ String ]
 }, {
   versionKey: false
 })
 
-const Dietary = mongoose.model( dbTables.Dietary, DietarySchema );
+const Dietary = mongoose.model( dbTables.DIETARY, DietarySchema );
 
 export default Dietary;

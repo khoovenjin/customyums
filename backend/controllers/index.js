07526500e9d7f61f172c0c90bbf6cffd96c9821f
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-07-29 22:34:28 +0800

    Added index for Controller

---
+++ backend/controllers/index.js
@@ -0,0 +1,9 @@
+import DietaryController from "./dietaryController.js";
+import PantryController from "./pantryController.js";
+import UserController from "./userController.js";
+
+export{
+  DietaryController,
+  PantryController,
+  UserController
+};

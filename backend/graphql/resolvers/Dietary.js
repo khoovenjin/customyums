f740192f135a415f417dbb6e78019a1814e9c3c4
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-07-30 15:20:02 +0800

    Created graphql dietary resolvers

---
+++ backend/graphql/resolvers/Dietary.js
@@ -0,0 +1,15 @@
+export const DietaryResolver = {
+  user: async ({ user_id }, args, { cache, controllers, serviceWorker }) => {
+    const req = {
+      params: {
+        id: user_id
+      }
+    }
+
+    return await serviceWorker.fetchResponse(
+      async () => await cache.fetchUser( user_id ),
+      async ( data ) => cache.storeUserExpire( user_id, data ),
+      async () => await controllers.UserController.apiGetUsersById( req ).then( res => res.at( 0 ) )
+    );
+  }
+}

93d47e0d3298caabc797ad519fd9e2e67746067f
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-07-30 15:22:40 +0800

    Created index for REST

---
+++ backend/routes/index.js
@@ -0,0 +1,9 @@
+import userRouter from "./userRoute.js";
+import pantryRouter from "./pantryRoute.js";
+import dietaryRouter from "./dietaryRoute.js";
+
+export{
+  userRouter,
+  pantryRouter,
+  dietaryRouter
+};

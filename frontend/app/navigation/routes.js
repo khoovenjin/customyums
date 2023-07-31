0c23c7d05d788bbd526a50327816de14e71641f3
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-07-31 15:53:45 +0800

    Created routes for navigation screen names

---
+++ frontend/app/navigation/routes.js
@@ -0,0 +1,12 @@
+export default Object.freeze({
+  // Login Stack
+  LOGIN: 'Login',
+  REGISTER: 'Register',
+
+  // General
+  DASHBOARD: 'Dashboard',
+  RECIPE: 'Recipe',
+  DIETARY: 'Dietary',
+  PANTRY: 'Pantry',
+  ADDPANTRY: 'AddPatry'
+})

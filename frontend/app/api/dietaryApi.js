a86e3da50274701c5b28206173610bb0ebf7f428
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-08-01 13:45:47 +0800

    Added userApi with all dietary-related methods

---
+++ frontend/app/api/dietaryApi.js
@@ -0,0 +1,47 @@
+import { gql } from "@apollo/client";
+
+const GET_DIETARIES = gql`
+  query getDietaries {
+    dietaries {
+      dietary_id
+      meal
+      recipes
+      date
+    }
+  }
+`;
+
+const GET_DIETARIES_BY_ID = gql`
+  query getDietaryById( $dietaryId: ID! ) {
+    dietary( id: $dietaryId ) {
+      dietary_id
+      meal
+      recipes
+      date
+    }
+  }
+`;
+
+const ADD_DIETARY = gql`
+  mutation addDietary( $input: DietaryInput! ){
+    addDietary( input: $input ){
+      dietary_id
+      meal
+      recipes
+      date
+    }
+  }
+`;
+
+const DELETE_DIETARY = gql`
+  mutation deleteDietary( $deleteDietaryId: ID!, $input: RecipeInput! ){
+    deleteDietary( id: $deleteDietaryId, input: $input )
+  }
+`
+
+export default{
+  GET_DIETARIES,
+  GET_DIETARIES_BY_ID,
+  ADD_DIETARY,
+  DELETE_DIETARY
+}

d776cd70a75f2207e34a0e7b05051545c5cb84f6
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-08-01 13:50:25 +0800

    Added hashPassword utility

--- backend/controllers/utils.js
+++ backend/controllers/utils.js
@@ -1,3 +1,10 @@
+import Security from "../security/index.js";
+
 export default class utilsController {
   static setDate = ( variable ) => variable.createdAt = 'createdAt' in variable? new Date( variable?.createdAt ) : new Date();
+
+  static hashPassword = ( variable ) => {
+    if( 'password' in variable )
+      variable.password = Security.hash( variable.password );
+  }
 }

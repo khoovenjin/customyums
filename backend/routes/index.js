a815b4c06b20ab950c86c095bea94330ee68e398
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-08-02 16:30:02 +0800

    Import over recipe Router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 import userRouter from "./userRoute.js";
 import pantryRouter from "./pantryRoute.js";
 import dietaryRouter from "./dietaryRoute.js";
+import recipeRouter from "./com.spoonacular/recipeRoute.js";
 
 export{
   userRouter,
   pantryRouter,
-  dietaryRouter
+  dietaryRouter,
+  recipeRouter
 };

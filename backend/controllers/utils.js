6e64389d2919726bd3dca5ddc4eb1cc6b701a2c4
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-08-04 15:53:36 +0800

    Added setCompleted function to set default isCompleted to false if payload property is not provided

--- backend/controllers/utils.js
+++ backend/controllers/utils.js
@@ -3,6 +3,8 @@ import Security from "../security/index.js";
 export default class utilsController {
   static setDate = ( variable ) => variable.createdAt = 'createdAt' in variable? new Date( variable?.createdAt ) : new Date();
 
+  static setCompleted = ( variable ) => variable.isCompleted = 'isCompleted' in variable? variable?.isCompleted : false;
+
   static hashPassword = ( variable ) => {
     if( 'password' in variable )
       variable.password = Security.hash( variable.password );

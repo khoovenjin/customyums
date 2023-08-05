75f62a0f91d16843cae51e2ff1f3dd026905dfb5
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-08-05 08:15:13 +0800

    Refactor matchKeyPair function, added skip and limit functions, and created a comprehensive query aggregator function.

--- backend/query/pipeline.js
+++ backend/query/pipeline.js
@@ -8,6 +8,22 @@ export default class dbQuery{
     date: ( variable ) => this.toDate( variable )
   }
 
+  static #attr_data_types = {
+    user_id: 'id'
+  }
+
+  static #date_operators = [
+    'gt',
+    'gte',
+    'lt',
+    'lte'
+  ]
+
+  static #postFix_pipeline = [
+    'skip',
+    'limit'
+  ]
+
   static createPipeline = ( pipeline ) => {
     if( payloadChecker.typeChecker( false, pipeline.data, 'null' ) )
       throw new Error('Invalid pipeline data-type. Recommend to call initPipeline().')
@@ -28,25 +44,72 @@ export default class dbQuery{
       return this.createPipeline( parentPipeline );
     }
 
-    // To filter data based on Several Attribute value (Aggregate)
+    // To filter data based on Several Attribute value (Aggregate; Only specific use)
     parentPipeline.matchKeyPair = ( hashMap, attrFormatObj ) => {
       let matchQuery = new Object();
 
       for( const [ key, value ] of Object.entries( hashMap ) ) {
         let propsValue = value;
 
-        if( key in attrFormatObj ) {
-          if( attrFormatObj[ key ] in this.#data_types )
-            propsValue = this.#data_types[ attrFormatObj[ key ] ]( value );
-          else
-            propsValue = value;
-        }
+        if( key in attrFormatObj )
+          propsValue = attrFormatObj[ key ] in this.#data_types?
+            this.#data_types[ attrFormatObj[ key ] ]( value ) : value
         
         matchQuery[ key ] = propsValue;
       }
       
       parentPipeline?.data.push( matchQuery );
         
+      return this.createPipeline( parentPipeline );
+    }
+
+    // Comprehensive query constructor for prefix, infix and postfix queries (includes: limit & skip)
+    parentPipeline.query = ( queries, infixPipeline ) => {
+      let query = {
+        $match: {
+          $and: []
+        }
+      }
+      let postFixQuery = new Array();
+
+      if( payloadChecker.typeChecker( false, queries, 'object' ) && Object.keys( queries ).length ) {
+        let queryKeys = Object.keys( queries );
+        let dateQuery = {
+          date: {}
+        };
+        let matchQuery = new Array();
+
+        queryKeys.forEach(( item ) => {
+          if( this.#date_operators.includes( item ) )
+            dateQuery.date[ `$${ item }` ] = new Date( queries[ item ] );
+          else if( this.#postFix_pipeline.includes( item ) )
+            postFixQuery.push({
+              [ `$${ item }` ]: parseInt( queries[ item ] )
+            })
+          else
+            matchQuery.push({
+              [ item ]: {
+                $eq: item in this.#attr_data_types?
+                  this.#data_types[ this.#attr_data_types[ item ] ]( queries[ item ] ) : queries[ item ]
+              }
+            })
+        })
+
+        if( matchQuery.length )
+          query.$match.$and.push( ...matchQuery );
+
+        if( Object.keys( dateQuery.date ).length )
+          query.$match.$and.push( dateQuery );
+      }
+
+      if( query.$match.$and.length )
+        parentPipeline?.data.push( query );
+
+      parentPipeline?.data.push( ...infixPipeline );
+
+      if( postFixQuery.length )
+        parentPipeline?.data.push( ...postFixQuery );
+      
       return this.createPipeline( parentPipeline );
     }
 
@@ -95,6 +158,34 @@ export default class dbQuery{
       }
 
       parentPipeline.data.push( pushQuery );
+
+      return this.createPipeline( parentPipeline );
+    }
+
+    // To skip data
+    parentPipeline.skip = ( number ) => {
+      if( payloadChecker.typeChecker( true, number, 'integer' ) )
+        return this.createPipeline( parentPipeline );
+
+      const skipQuery = {
+        $skip: number
+      }
+
+      parentPipeline.data.push( skipQuery );
+
+      return this.createPipeline( parentPipeline );
+    }
+
+    // To limit data
+    parentPipeline.limit = ( number ) => {
+      if( payloadChecker.typeChecker( true, number, 'integer' ) )
+        return this.createPipeline( parentPipeline );
+        
+      const limitQuery = {
+        $limit: number
+      }
+
+      parentPipeline.data.push( limitQuery );
 
       return this.createPipeline( parentPipeline );
     }

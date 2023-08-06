4b7bfdde06bc674bc4436bb9794a6450473a0aef
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-08-06 09:09:17 +0800

    Removed caching due to query complexity and added grapqhl middlware type checking for query params

--- backend/graphql/resolvers/Query.js
+++ backend/graphql/resolvers/Query.js
@@ -1,13 +1,19 @@
 import { graphQL as middlewareGraphQL } from "../../middleware/index.js";
 
 export const QueryResolver = {
-  dietaries: async ( parent, args, { cache, controllers, serviceWorker }) => {
-    return await serviceWorker.fetchResponse(
-      async () => await cache.fetchDietaries(),
-      async ( data ) => await cache.storeDietariesExpire( data ),
-      async () => await controllers.DietaryController.apiGetDietaries()
-    ).catch( error => console.log(`Unable to execute serviceWorker.`) );
-  },
+  dietaries: middlewareGraphQL.validateReqQuery( 'dietary' ).createResolver( async ( parent, { query }, { cache, controllers, serviceWorker }) => {
+    const req = {
+      query
+    };
+
+    // return await serviceWorker.fetchResponse(
+    //   async () => await cache.fetchDietaries(),
+    //   async ( data ) => await cache.storeDietariesExpire( data ),
+    //   async () => await controllers.DietaryController.apiGetDietaries( req )
+    // ).catch( error => console.log(`Unable to execute serviceWorker.`) );
+
+    return await controllers.DietaryController.apiGetDietaries( req );
+  }),
   dietary: middlewareGraphQL.validateReqParamsId.createResolver( async ( parent, { id }, { cache, controllers, serviceWorker }) => {
     const req = {
       params: {
@@ -21,13 +27,19 @@ export const QueryResolver = {
       async () => await controllers.DietaryController.apiGetDietariesById( req ).then( res => res.at( 0 ) )
     ).catch( error => console.log(`Unable to execute serviceWorker.`) );
   }),
-  pantries: async ( parent, args, { cache, controllers, serviceWorker }) => {
-    return await serviceWorker.fetchResponse(
-      async () => await cache.fetchPantries(),
-      async ( data ) => await cache.storePantriesExpire( data ),
-      async () => await controllers.PantryController.apiGetPantries()
-    ).catch( error => console.log(`Unable to execute serviceWorker.`) );
-  },
+  pantries: middlewareGraphQL.validateReqQuery( 'pantry' ).createResolver( async ( parent, { query }, { cache, controllers, serviceWorker }) => {
+    const req = {
+      query
+    };
+
+    // return await serviceWorker.fetchResponse(
+    //   async () => await cache.fetchPantries(),
+    //   async ( data ) => await cache.storePantriesExpire( data ),
+    //   async () => await controllers.PantryController.apiGetPantries( req )
+    // ).catch( error => console.log(`Unable to execute serviceWorker.`) );
+
+    return await controllers.PantryController.apiGetPantries( req );
+  }),
   pantry: middlewareGraphQL.validateReqParamsId.createResolver( async ( parent, { id }, { cache, controllers, serviceWorker }) => {
     const req = {
       params: {
@@ -41,13 +53,19 @@ export const QueryResolver = {
       async () => await controllers.PantryController.apiGetPantriesById( req ).then( res => res.at( 0 ) )
     ).catch( error => console.log(`Unable to execute serviceWorker.`) );
   }),
-  users: async ( parent, args, { cache, controllers, serviceWorker }) => {
-    return await serviceWorker.fetchResponse(
-      async () => await cache.fetchUsers(),
-      async ( data ) => await cache.storeUsersExpire( data ),
-      async () => await controllers.UserController.apiGetUsers()
-    ).catch( error => console.log(`Unable to execute serviceWorker.`) );
-  },
+  users: middlewareGraphQL.validateReqQuery( 'user' ).createResolver( async ( parent, { query }, { cache, controllers, serviceWorker }) => {
+    const req = {
+      query
+    };
+
+    // return await serviceWorker.fetchResponse(
+    //   async () => await cache.fetchUsers(),
+    //   async ( data ) => await cache.storeUsersExpire( data ),
+    //   async () => await controllers.UserController.apiGetUsers( req )
+    // ).catch( error => console.log(`Unable to execute serviceWorker.`) );
+
+    return await controllers.UserController.apiGetUsers( req );
+  }),
   user: middlewareGraphQL.validateReqParamsId.createResolver( async ( parent, { id }, { cache, controllers, serviceWorker }) => {
     const req = {
       params: {

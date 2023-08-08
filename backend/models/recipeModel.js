e7fd315ecf88ae7d2cbf7f18069834535caeeaef
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-08-08 11:34:09 +0800

    Refactor recipe model schema

--- backend/models/recipeModel.js
+++ backend/models/recipeModel.js
@@ -1,11 +1,18 @@
 import mongoose from "mongoose";
 
+import NutritionSchema from "./nutritionModel.js";
+import IngredientSchema from "./ingredientModel.js";
+
 const RecipeSchema = new mongoose.Schema({
   recipe_id: String,
   title: String,
   description: String,
   image: String,
-  tags: [ String ]
+  tags: [ String ], // Uses cuisine, meal types and diet types
+  time: Number,
+  nutrition: [ NutritionSchema ],
+  ingredients: [ IngredientSchema ],
+  steps: [ String ]
 }, {
   versionKey: false
 })

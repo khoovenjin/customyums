fea242624985a342d0af1b4d27c93eb923381596
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-08-09 18:37:42 +0800

    Include recipeController into controller index

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,9 +1,11 @@
 import DietaryController from "./dietaryController.js";
 import PantryController from "./pantryController.js";
 import UserController from "./userController.js";
+import RecipeController from "./com.spoonacular/recipeController.js";
 
 export{
   DietaryController,
   PantryController,
-  UserController
+  UserController,
+  RecipeController
 };

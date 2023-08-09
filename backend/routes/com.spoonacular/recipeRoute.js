60eee3743dcabfbce3f9492056f3a047f354ff43
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-08-09 18:44:17 +0800

    Refactor recipe routes to include middleware for payload, params, and query checking

--- backend/routes/com.spoonacular/recipeRoute.js
+++ backend/routes/com.spoonacular/recipeRoute.js
@@ -1,13 +1,41 @@
 import express from "express";
 
-import RecipeController from "../../controllers/com.spoonacular/recipeController.js";
+import { RecipeController } from "../../controllers/index.js";
+import { restAPI as middlewareRestAPI } from "../../middleware/index.js";
 
 const recipeRouter = express.Router();
 
-recipeRouter.get( "/recipe/:id/information", RecipeController.apiGetRecipeById_Information );
+recipeRouter.get(
+  "/recipe/:id/information",
+  // Middleware
+  middlewareRestAPI.validateReqParamsId,
+  // Controller -> DAO
+  RecipeController.apiGetRecipeById_Information
+);
 
-recipeRouter.get( "/recipe/autocomplete", RecipeController.apiGetRecipeMatchByKey );
+recipeRouter.get(
+  "/recipe/autocomplete",
+  // Middleware
+  middlewareRestAPI.validateReqPayload( 'spoonacular_query_key' ),
+  // Controller -> DAO
+  RecipeController.apiGetRecipeMatchByKey
+);
 
-recipeRouter.get( "/recipe/:id/instruction", RecipeController.apiGetRecipeById_Instruction );
+recipeRouter.get(
+  "/recipe/:id/instruction",
+  // Middleware
+  middlewareRestAPI.validateReqParamsId,
+  // Controller -> DAO
+  RecipeController.apiGetRecipeById_Instruction
+);
+
+recipeRouter.get(
+  "/recipe/ingredients",
+  // Middleware
+  middlewareRestAPI.validateReqPayload( 'spoonacular_body_ingredients' ),
+  middlewareRestAPI.validateReqQuery( 'spoonacular_query_number' ),
+  // Controller -> DAO
+  RecipeController.apiGetRecipeByIngredients
+);
 
 export default recipeRouter;

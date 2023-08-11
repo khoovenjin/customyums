592b4d2c8b19f17359eddc1d7b419c4727e34559
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-08-12 00:04:39 +0800

    Refactor utilities

--- frontend/utilities/utils.js
+++ frontend/utilities/utils.js
@@ -1,12 +1,45 @@
 import moment from "moment";
 
-import { payloadChecker } from "./payloadChecker";
-
 export default class Utils {
   static #dateFormat = 'YYYY-MM-DD';
 
   static dateToString = ( date ) => moment( date ).format( this.#dateFormat );
 
-  static arrayToString = ( data ) => payloadChecker.typeChecker( false, data, 'array' )?
+  static arrayToString = ( data ) => this.typeChecker( false, data, 'array' )?
                                         data.join( ',' ) : data;
+  
+  static typeChecker = (
+    isInverse = false,
+    variable,
+    type
+  ) => {
+    const typeExpression = {
+      null: () => typeof variable === 'undefined' || variable === null,
+      string: () => typeof variable === 'string' || variable instanceof String,
+      integer: () => Number.isFinite( variable ),
+      boolean: () => typeof variable === 'boolean',
+      date: () => variable instanceof Date,
+      // ISO 8601 - YYYY - MM - DD
+      dateFormat: () => {
+        let regex = /^\d{4}-\d{2}-\d{2}$/;
+  
+        if( !variable.match( regex ) ) return false;
+        
+        let date = new Date( variable );
+        let dateNum = date.getTime();
+  
+        if( !dateNum && dateNum !== 0 ) return false;
+  
+        return date.toISOString().slice( 0, 10 ) === variable;
+      },
+      array: () => Array.isArray( variable ),
+      object: () => typeof variable === 'object' && !Array.isArray( variable ) && variable !== null
+    };
+  
+    const expression = typeExpression[ type ]();
+    
+    if( isInverse ) return !expression;
+  
+    return expression;
+  }
 }

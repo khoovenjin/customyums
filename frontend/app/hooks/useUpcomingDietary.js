a5a663643bcdbf8511afd19a65c70e66b5803e6e
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-08-13 16:14:22 +0800

    Refactor memoized query

--- frontend/app/hooks/useUpcomingDietary.js
+++ frontend/app/hooks/useUpcomingDietary.js
@@ -6,14 +6,14 @@ import Utils from '../../utilities/utils';
 
 export default useUpcomingDietary = () => {
   const currentDate = Utils.dateToString( Date.now() );
-  const getQuery = useMemo(() => ({ query: { isCompleted: false, gte: currentDate, nearest: true } }), []);
+  const query = useMemo(() => ({ query: { isCompleted: false, gte: currentDate, nearest: true } }), []);
 
   const {
     loading: dietariesLoading,
     error: dietariesError,
     data: upcomingDietaries
   } = useQuery( dietaryApi.GET_DIETARIES, {
-    variables: getQuery
+    variables: query
   })
 
   return [ dietariesLoading, dietariesError, upcomingDietaries ];

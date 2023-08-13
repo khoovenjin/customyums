83228187c9882cbb514ee5c2fe0ad35d743bcbae
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-08-13 16:16:11 +0800

    Created use search recipe custom hooks

---
+++ frontend/app/hooks/useSearchRecipe.js
@@ -0,0 +1,36 @@
+import { useDeferredValue, useEffect, useMemo, useState } from 'react';
+
+import spoonacularApi from '../api/com.spoonacular/client';
+
+import Utils from '../../utilities/utils';
+
+export default useSearchRecipe = () => {
+  const [ searchQuery, setSearchQuery ] = useState( '' );
+  const [ searchRecipes, setSearchRecipes ] = useState([]);
+  const deferredRecipes = useDeferredValue( searchRecipes );
+  const timeout = 2000; // 2 sec
+  const minLength = 3;
+
+  const delayDebounce = useMemo(() => {
+    if( searchQuery.length >= minLength ) {
+      return setTimeout( async () => {
+        const { data } = await spoonacularApi.GET_RECIPE_MATCH_BY_KEY( searchQuery );
+  
+        setSearchRecipes( data );
+      }, timeout );
+    }
+    
+    return null;
+  }, [ searchQuery ]);
+
+  useEffect(() => {
+    let timeoutId;
+
+    timeoutId = delayDebounce;
+    
+    if( Utils.typeChecker( true, timeoutId, 'null' ) )
+      return () => clearTimeout( timeoutId );
+  }, [ searchQuery ]);
+
+  return [ deferredRecipes, setSearchQuery ];
+}
